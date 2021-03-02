89913d5994193e7954ef08da6dfcdf5279579305
Scott Morrison <scott.morrison@gmail.com>
2021-03-02 21:57:57 +1100

    copy get_architecture() from latest rustup-init.sh

--- elan-init.sh
+++ elan-init.sh
@@ -180,51 +180,78 @@ get_endianness() {
 }
 
 get_architecture() {
-
-    local _ostype="$(uname -s)"
-    local _cputype="$(uname -m)"
+    local _ostype _cputype _bitness _arch _clibtype
+    _ostype="$(uname -s)"
+    _cputype="$(uname -m)"
+    _clibtype="gnu"
 
     if [ "$_ostype" = Linux ]; then
         if [ "$(uname -o)" = Android ]; then
-            local _ostype=Android
+            _ostype=Android
+        fi
+        if ldd --version 2>&1 | grep -q 'musl'; then
+            _clibtype="musl"
         fi
     fi
 
-    if [ "$_ostype" = Darwin -a "$_cputype" = i386 ]; then
-        # Darwin `uname -s` lies
+    if [ "$_ostype" = Darwin ] && [ "$_cputype" = i386 ]; then
+        # Darwin `uname -m` lies
         if sysctl hw.optional.x86_64 | grep -q ': 1'; then
-            local _cputype=x86_64
+            _cputype=x86_64
+        fi
+    fi
+
+    if [ "$_ostype" = SunOS ]; then
+        # Both Solaris and illumos presently announce as "SunOS" in "uname -s"
+        # so use "uname -o" to disambiguate.  We use the full path to the
+        # system uname in case the user has coreutils uname first in PATH,
+        # which has historically sometimes printed the wrong value here.
+        if [ "$(/usr/bin/uname -o)" = illumos ]; then
+            _ostype=illumos
+        fi
+
+        # illumos systems have multi-arch userlands, and "uname -m" reports the
+        # machine hardware name; e.g., "i86pc" on both 32- and 64-bit x86
+        # systems.  Check for the native (widest) instruction set on the
+        # running kernel:
+        if [ "$_cputype" = i86pc ]; then
+            _cputype="$(isainfo -n)"
         fi
     fi
 
     case "$_ostype" in
 
         Android)
-            local _ostype=linux-android
+            _ostype=linux-android
             ;;
 
         Linux)
-            local _ostype=unknown-linux-gnu
+            _ostype=unknown-linux-$_clibtype
+            _bitness=$(get_bitness)
             ;;
 
         FreeBSD)
-            local _ostype=unknown-freebsd
+            _ostype=unknown-freebsd
             ;;
 
         NetBSD)
-            local _ostype=unknown-netbsd
+            _ostype=unknown-netbsd
             ;;
 
         DragonFly)
-            local _ostype=unknown-dragonfly
+            _ostype=unknown-dragonfly
             ;;
 
         Darwin)
-            local _ostype=apple-darwin
+            _ostype=apple-darwin
+            ;;
+
+        illumos)
+            _ostype=unknown-illumos
             ;;
 
         MINGW* | MSYS* | CYGWIN*)
-            local _ostype=pc-windows-msvc
+            _ostype=pc-windows-gnu
             ;;
 
         *)
@@ -236,97 +263,114 @@ get_architecture() {
     case "$_cputype" in
 
         i386 | i486 | i686 | i786 | x86)
-            local _cputype=i686
+            _cputype=i686
             ;;
 
         xscale | arm)
-            local _cputype=arm
+            _cputype=arm
             if [ "$_ostype" = "linux-android" ]; then
-                local _ostype=linux-androideabi
+                _ostype=linux-androideabi
             fi
             ;;
 
         armv6l)
-            local _cputype=arm
+            _cputype=arm
             if [ "$_ostype" = "linux-android" ]; then
-                local _ostype=linux-androideabi
+                _ostype=linux-androideabi
             else
-                local _ostype="${_ostype}eabihf"
+                _ostype="${_ostype}eabihf"
             fi
             ;;
 
         armv7l | armv8l)
-            local _cputype=armv7
+            _cputype=armv7
             if [ "$_ostype" = "linux-android" ]; then
-                local _ostype=linux-androideabi
+                _ostype=linux-androideabi
             else
-                local _ostype="${_ostype}eabihf"
+                _ostype="${_ostype}eabihf"
             fi
             ;;
 
-        aarch64)
-            local _cputype=aarch64
+        aarch64 | arm64)
+            _cputype=aarch64
             ;;
 
         x86_64 | x86-64 | x64 | amd64)
-            local _cputype=x86_64
+            _cputype=x86_64
             ;;
 
         mips)
-            local _cputype="$(get_endianness $_cputype "" 'el')"
+            _cputype=$(get_endianness mips '' el)
             ;;
 
         mips64)
-            local _bitness="$(get_bitness)"
-            if [ $_bitness = "32" ]; then
-                if [ $_ostype = "unknown-linux-gnu" ]; then
-                    # 64-bit kernel with 32-bit userland
-                    # endianness suffix is appended later
-                    local _cputype=mips
-                fi
-            else
+            if [ "$_bitness" -eq 64 ]; then
                 # only n64 ABI is supported for now
-                local _ostype="${_ostype}abi64"
+                _ostype="${_ostype}abi64"
+                _cputype=$(get_endianness mips64 '' el)
             fi
-
-            local _cputype="$(get_endianness $_cputype "" 'el')"
             ;;
 
         ppc)
-            local _cputype=powerpc
+            _cputype=powerpc
             ;;
 
         ppc64)
-            local _cputype=powerpc64
+            _cputype=powerpc64
             ;;
 
         ppc64le)
-            local _cputype=powerpc64le
+            _cputype=powerpc64le
             ;;
 
+        s390x)
+            _cputype=s390x
+            ;;
+        riscv64)
+            _cputype=riscv64gc
+            ;;
         *)
             err "unknown CPU type: $_cputype"
 
     esac
 
     # Detect 64-bit linux with 32-bit userland
-    if [ $_ostype = unknown-linux-gnu -a $_cputype = x86_64 ]; then
-        if [ "$(get_bitness)" = "32" ]; then
-            local _cputype=i686
-        fi
+    if [ "${_ostype}" = unknown-linux-gnu ] && [ "${_bitness}" -eq 32 ]; then
+        case $_cputype in
+            x86_64)
+                _cputype=i686
+                ;;
+            mips64)
+                _cputype=$(get_endianness mips '' el)
+                ;;
+            powerpc64)
+                _cputype=powerpc
+                ;;
+            aarch64)
+                _cputype=armv7
+                if [ "$_ostype" = "linux-android" ]; then
+                    _ostype=linux-androideabi
+                else
+                    _ostype="${_ostype}eabihf"
+                fi
+                ;;
+            riscv64gc)
+                err "riscv64 with 32-bit userland unsupported"
+                ;;
+        esac
     fi
 
-    # Detect armv7 but without the CPU features Lean needs in that build,
+    # Detect armv7 but without the CPU features Rust needs in that build,
     # and fall back to arm.
-    # See https://github.com/rust-lang-nursery/rustup.rs/issues/587.
-    if [ $_ostype = "unknown-linux-gnueabihf" -a $_cputype = armv7 ]; then
+    # See https://github.com/rust-lang/rustup.rs/issues/587.
+    if [ "$_ostype" = "unknown-linux-gnueabihf" ] && [ "$_cputype" = armv7 ]; then
         if ensure grep '^Features' /proc/cpuinfo | grep -q -v neon; then
             # At least one processor does not have NEON.
-            local _cputype=arm
+            _cputype=arm
         fi
     fi
 
-    local _arch="$_cputype-$_ostype"
+    _arch="${_cputype}-${_ostype}"
 
     RETVAL="$_arch"
 }
